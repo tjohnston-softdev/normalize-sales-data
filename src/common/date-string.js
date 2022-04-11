4c1753c9cd4bc0f67f95fba6f6cdc4fd957e9528
tjohnston-softdev <78240416+tjohnston-softdev@users.noreply.github.com>
2022-04-11 17:08:28 +1000

    Cleaned up 'date-string'

--- src/common/date-string.js
+++ src/common/date-string.js
@@ -1,34 +1,21 @@
 // This file converts a date object into a string. (eg. 2021-03-21)
 
-
 // Main function.
 function writeDateStringFromObject(dateObj)
-{
-	var yearNumber = -1;
-	var monthNumber = -1;
-	var dayNumber = -1;
-	
-	var yearString = "";
-	var monthString = "";
-	var dayString = "";
-	
-	var writeRes = "";
-	
-	
+{	
 	// Reads date numbers.
-	yearNumber = dateObj.getFullYear();
-	monthNumber = dateObj.getMonth() + 1;
-	dayNumber = dateObj.getDate();
+	var yearNumber = dateObj.getFullYear();
+	var monthNumber = dateObj.getMonth() + 1;
+	var dayNumber = dateObj.getDate();
 	
 	// Casts to string.
-	yearString = String(yearNumber);
-	monthString = formatDateNumber(monthNumber);
-	dayString = formatDateNumber(dayNumber);
+	var yearString = String(yearNumber);
+	var monthString = formatDateNumber(monthNumber);
+	var dayString = formatDateNumber(dayNumber);
 	
 	
-	// Writes full date.
-	writeRes = [yearString, monthString, dayString].join("-");
-	return writeRes;
+	// Writes and returns full date.
+	return [yearString, monthString, dayString].join("-");
 }
 
 
@@ -36,20 +23,7 @@ function writeDateStringFromObject(dateObj)
 // Formats as two-digit number.
 function formatDateNumber(origNum)
 {
-	var formatRes = "";
-	
-	if (origNum >= 10)
-	{
-		// Use as-is.
-		formatRes = String(origNum);
-	}
-	else
-	{
-		// Add zero.
-		formatRes = "0" + origNum;
-	}
-	
-	return formatRes;
+	return (origNum >= 10) ? String(origNum) : ("0" + origNum)
 }
 
 
